1dd3d0656c074b2fd66ac5ae6265a5cd4c67ab43
kishore1278 <yuvadonadi17@gmail.com>
2023-08-01 20:03:26 +0530

    adding new dp

--- dp.sh
+++ dp.sh
@@ -4,3 +4,17 @@ sudo apt update
 sudo apt install wget apache2 unzip -y
 systemctl start apache2
 systemctl enable apache2
+sudo apt install apache2 \
+                 ghostscript \
+                 libapache2-mod-php \
+                 mysql-server \
+                 php \
+                 php-bcmath \
+                 php-curl \
+                 php-imagick \
+                 php-intl \
+                 php-json \
+                 php-mbstring \
+                 php-mysql \
+                 php-xml \
+                 php-zip -y
